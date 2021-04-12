8496d966dd9f76055cc5087222cc778e5a6da308
tadeumatos <tadeu.matos@gmail.com>
2021-04-12 15:00:36 -0300

    Update class

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,15 +1,13 @@
 using API_ALB.Models;
 using Microsoft.EntityFrameworkCore;
-
 namespace API_ALB.Context
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
-        :base(options)
-        {            
-        }
-        public DbSet<Customer> Customers{get;set;}
-        
+        :base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
     }
 }
